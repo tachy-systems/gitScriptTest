94b8e00eddf2c9f9cd90816a0ad32672b7343a48
MadhanKumar-1 <madhan@teams.tachyonsys.com.au>
2024-03-08 19:34:28 +0530

    Initial commit

--- apiCodeGen.sh
+++ apiCodeGen.sh
@@ -5,3 +5,20 @@ cd ../2_CodeProducer
 npm run start
 cd ../6_Deployer
 npm run start
+
+
+
+search_dir="../"
+
+repoName="$1"
+token="$2"
+
+
+find "$search_dir" -type d -wholename "*/cnyp*/lambda" | while read -r dir; do
+    if [ -f "$dir/createRepo.sh" ]; then
+        echo "Running script in directory: $dir"
+        bash "$dir/createRepo.sh" "$repoName" "$token" "$dir"
+    else
+        echo "No script found in directory: $dir"
+    fi
+done
--- git.sh
+++ git.sh
@@ -1,16 +1,13 @@
-
 search_dir="../"
 
 repoName="$1"
-
-# Second parameter provided by the user
 token="$2"
 
 
 find "$search_dir" -type d -wholename "*/cnyp*/lambda" | while read -r dir; do
     if [ -f "$dir/createRepo.sh" ]; then
         echo "Running script in directory: $dir"
-        bash "$dir/createRepo.sh" "$repoName" "$token"
+        bash "$dir/createRepo.sh" "$repoName" "$token" "$dir"
     else
         echo "No script found in directory: $dir"
     fi
